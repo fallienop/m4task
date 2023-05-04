31defe3180b93f136be2ef0d3c6c86689ff0cea7
sahin <sahin@FALLIEN>
2023-05-04 13:17:35 +0400

    Add project files.

---
+++ m4task/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace oopint;
+using m4task;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+    go:
+        Console.Write("Enter name:");
+        string fn=Console.ReadLine();
+        Console.Write("Enter email:");
+        string email = Console.ReadLine();
+        Console.Write("Enter password:");
+        string password = Console.ReadLine();
+        
+        try
+        {
+            User u = new User(fn,email,password);
+            Console.WriteLine(u.ShowInfo());
+        }
+        catch (Exception ex)
+        {
+
+           Console.WriteLine(ex.Message);
+        }
+        Console.WriteLine("New? y or n");
+        string c=Console.ReadLine().ToLower();
+
+        if (c =="y" )
+        {
+            goto go;
+        }
+     
+
+
+        Console.ReadLine();
+    }
+}
---
+++ m4task/interface.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m4task
+{
+    public interface IAccount
+    {
+        public bool PasswordChecker(string password);
+
+
+        public string ShowInfo();
+    }
+
+    public class User : IAccount
+    {
+        private static byte cnt=0;
+        public byte Id { get; }  
+        public string Fullname    ;
+        public string Email       ;
+        public string Password;
+        public User(string fullname,string email,string password)
+        {
+            if (PasswordChecker(password))
+            {
+                this.Password = password;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid password format.");
+            }
+            this.Id = ++cnt;
+            this.Fullname = fullname;   
+            this.Email = email;
+           
+        }
+
+        public bool PasswordChecker(string password)
+        {
+            if (password.Length < 8) { return false; }
+            if (!password.Any(char.IsUpper)){ return false; }
+           else if(!password.Any(char.IsLower)){ return false; }
+            else  if(!password.Any(char.IsNumber)){ return false; }
+            else { return true; }
+        }
+
+        public string ShowInfo()
+        {
+            return this.Id+" "+this.Fullname+" "+this.Email;
+        }
+    }
+}
